65089cba63cf0f0c4bb9aba45e51b86c04e770e7
Scumbrum <73284043+Scumbrum@users.noreply.github.com>
2021-02-21 11:36:06 +0200

    add task 3

---
+++ lab_14/Main.java
@@ -0,0 +1,51 @@
+package lab_14;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		try {
+			copyNoEmptyStrings("C:\\Users\\User\\eclipse-workspace\\hg.txt",
+					"C:\\Users\\User\\eclipse-workspace\\h.txt");
+			System.out.println("Success!!!");
+		} catch (IOException e) {
+			System.out.println("Exception: "+e.getMessage());
+		}catch(IllegalArgumentException e) {
+			System.out.println("Exception: "+e.getMessage());
+		}
+	}
+
+	public static void copyNoEmptyStrings(String source, String destination) throws IOException {
+		try(
+		FileReader input = new FileReader(source);
+		FileWriter output = new FileWriter(destination);){
+		int thread;
+		boolean start = true;
+		int count=0;
+		while ((thread = input.read()) != -1) {
+			count++;
+			char character = (char) thread;
+			if (character == 13 && start) {
+				continue;
+			}
+			if (character == 10) {
+				if (!start) {
+					output.write(character);
+				}
+				start = true;
+				continue;
+			}
+			start = false;
+			output.write(character);
+		}
+		if (count==0) {
+			throw new IllegalArgumentException("Empty file");
+		}
+		}catch(IOException ex) {
+			throw new IOException("not exist file", ex);
+		}
+	}
+}
