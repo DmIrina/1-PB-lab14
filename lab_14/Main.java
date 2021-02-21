31d6f8ba829d742b0796b8eeb83322dcc4abb909
Scumbrum <73284043+Scumbrum@users.noreply.github.com>
2021-02-21 16:34:03 +0200

    task3 optimization

--- lab_14/Main.java
+++ lab_14/Main.java
@@ -12,39 +12,40 @@ public class Main {
 					"C:\\Users\\User\\eclipse-workspace\\h.txt");
 			System.out.println("Success!!!");
 		} catch (IOException e) {
-			System.out.println("Exception: "+e.getMessage());
-		}catch(IllegalArgumentException e) {
-			System.out.println("Exception: "+e.getMessage());
+			System.out.println("Exception: " + e.getMessage());
+		} catch (IllegalArgumentException e) {
+			System.out.println("Exception: " + e.getMessage());
 		}
 	}
 
 	public static void copyNoEmptyStrings(String source, String destination) throws IOException {
-		try(
-		FileReader input = new FileReader(source);
-		FileWriter output = new FileWriter(destination);){
-		int thread;
-		boolean start = true;
-		int count=0;
-		while ((thread = input.read()) != -1) {
-			count++;
-			char character = (char) thread;
-			if (character == 13 && start) {
-				continue;
-			}
-			if (character == 10) {
-				if (!start) {
+		try (FileReader input = new FileReader(source); FileWriter output = new FileWriter(destination);) {
+			char[] buffer = new char[512];
+			int thread;
+			boolean start = true;
+			int count = 0;
+			while ((thread = input.read(buffer)) != -1) {
+				for (int i = 0; i < thread; i++) {
+					count++;
+					char character = (char) buffer[i];
+					if (character == 13 && start) {
+						continue;
+					}
+					if (character == 10) {
+						if (!start) {
+							output.write(character);
+						}
+						start = true;
+						continue;
+					}
+					start = false;
 					output.write(character);
 				}
-				start = true;
-				continue;
+				if (count == 0) {
+					throw new IllegalArgumentException("Empty file");
+				}
 			}
-			start = false;
-			output.write(character);
-		}
-		if (count==0) {
-			throw new IllegalArgumentException("Empty file");
-		}
-		}catch(IOException ex) {
+		} catch (IOException ex) {
 			throw new IOException("not exist file", ex);
 		}
 	}
